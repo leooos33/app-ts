1fa4326b5b6dc7007313a1449c07b123fb0ec105
leooos33 <sigmaw39@gmail.com>
2021-11-23 19:18:30 +0200

    sf

--- src/components/InputFrom.tsx
+++ src/components/InputFrom.tsx
@@ -4,8 +4,9 @@ import {ActionType} from "../types/stateType";
 
 const InputFrom = () => {
     const {state, changeState} = useContext(ContextApp);
+    const [inputFrom, setInputFrom] = React.useState()
     return (
-        <input className="text-right appearance-none bg-transparent border-none w-full text-white text-2xl font-semibold text-white mr-3 my-1 pr-3 leading-tight focus:outline-none " type="text" placeholder= {state?.fromAmount} 
+        <input id = "inputFrom" className="text-right appearance-none bg-transparent border-none w-full text-white text-2xl font-semibold text-white mr-3 my-1 pr-3 leading-tight focus:outline-none " type="text" placeholder = {state?.fromAmount != 0 ? (state?.fromAmount) : '0.0'} 
         onChange={(e) => changeState(({type: ActionType.CHANGE, 
             payload: {
             toToken: state?.toToken, 
