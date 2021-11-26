de81261d88201b08e2e357017669303b7f05a81a
leooos33 <sigmaw39@gmail.com>
2021-11-26 13:56:34 +0200

    sf

--- src/components/SwapButton.tsx
+++ src/components/SwapButton.tsx
@@ -5,6 +5,9 @@ const SwapButton = () => {
     const {state, changeState} = useContext(ContextSwap);
     return (
         <ButtonSwapContainer>
+
+        
+
         {state?.fromAmount !== 0 ? ('Confirm') : 'Enter an amount'}
         </ButtonSwapContainer>
     )
@@ -15,9 +18,9 @@ export default SwapButton
 const ButtonSwapContainer = ({children}) => {
     return (
         <div className="container mx-auto flex py-32 items-center justify-center flex-col">
-        <div className="text-center w-5/12">
+        <div className="text-center w-2/5">
             <div className="flex justify-center text-center">
-                <button className="bg-transparent w-full ml-14 border-gray1-g66 border text-orange1 font-sans font-medium text-xl pt-2 pb-3 px-4 rounded-lg">
+                <button className="bg-transparent w-full border-gray1-g66 border text-orange1 font-sans font-medium text-xl pt-2 pb-3 rounded-lg">
                     {children}
                 </button>
             </div>
